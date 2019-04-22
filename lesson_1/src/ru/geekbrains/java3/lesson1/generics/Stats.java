556dc80281572447ba693d06685fc7649a020824
via <via@wizardforum.ru>
2019-04-22 14:11:28 +0300

    задача 2; Написать метод, который преобразует массив в ArrayList;

--- lesson_1/src/ru/geekbrains/java3/lesson1/generics/Stats.java
+++ lesson_1/src/ru/geekbrains/java3/lesson1/generics/Stats.java
@@ -1,5 +1,7 @@
 package ru.geekbrains.java3.lesson1.generics;
 
+import java.util.ArrayList;
+
 public class Stats<T extends Integer> {
     public T[] items;
 
@@ -8,16 +10,30 @@ public class Stats<T extends Integer> {
     }
 
 
-//    public void changeArr(int a, int b){
-//        this.items[a] = b; //
-//    }
+    /**
+     * Задача 2. Написать метод, который преобразует массив в ArrayList;
+     *
+     * @return ArrayList
+     */
+    public ArrayList convertToArrayList() {
+
+        ArrayList arr = new ArrayList();
+        String str = new String();
+
+        for (int i = 0; i < items.length; i++) {
+            str = items[i].toString();
+            arr.add(str);
+        }
+
+        return arr;
+    }
 
     public static void main(String args[]) {
         Stats<Integer> intStats = new Stats<Integer>(1, 2, 3, 4, 5);
 
-        intStats.items[0] = 1;
+        System.out.println(intStats.convertToArrayList().toString());
 
-
+        System.out.println("Hello, World!");
     }
 
 
